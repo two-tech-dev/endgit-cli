8a6d2392f9af5fe74cb2f16295f4e48f5c1d22e6
Rezn1r <rezn1r@proton.me>
2026-05-07 14:57:11 +0800

    feat: add most function from search

--- cmd/search.go
+++ cmd/search.go
@@ -1,36 +1,89 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+Copyright © 2026 Two Tech Studio
 */
 package cmd
 
 import (
 	"fmt"
 	"strings"
+	"time"
 
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"github.com/two-tech-dev/endgit-cli/internal/api"
 )
 
-// searchCmd represents the search command
 var searchCmd = &cobra.Command{
 	Use:   "search <query>",
 	Short: "Search for plugins in the EndGit registry",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		query := strings.Join(args, " ")
-		fmt.Println("search called with query:", query)
+
+		s := spinner.New(spinner.CharSets[14], 120*time.Millisecond)
+		s.Suffix = fmt.Sprintf(" Searching for \"%s\"...", query)
+		s.Start()
+
+		client := api.NewClient()
+		response, err := client.GetPlugins(query)
+
+		s.Stop()
+
+		if err != nil {
+			color.Red("Search failed: %v", err)
+			return
+		}
+
+		plugins := response.Data.Plugins
+
+		if len(plugins) == 0 {
+			color.Yellow("No plugins found matching \"%s\".", query)
+			return
+		}
+
+		color.Cyan("\nFound %d plugin(s):\n", len(plugins))
+		color.HiBlack(strings.Repeat("-", 80))
+
+		for _, p := range plugins {
+			// type badge
+			typeStr := ""
+			if strings.ToUpper(p.PluginType) == "PYTHON" {
+				typeStr = color.HiGreenString("[Py]")
+			} else {
+				typeStr = color.HiBlueString("[C++]")
+			}
+
+			nameStr := color.New(color.FgHiWhite, color.Bold).
+				Sprintf("%-20s", p.Name)
+
+			versionStr := color.MagentaString("v%s", fallbackVersion(p.LatestVersion))
+			downloadsStr := color.YellowString("%d ⬇", p.Downloads)
+
+			fmt.Printf("%s %s | %s | %s\n",
+				typeStr,
+				nameStr,
+				versionStr,
+				downloadsStr,
+			)
+
+			fmt.Printf("%s\n", color.HiBlackString(p.Description))
+		}
+
+		color.HiBlack(strings.Repeat("-", 80))
+		color.White("\nRun %s to install.\n",
+			color.HiWhiteString("endgit install <plugin-name>"),
+		)
 	},
+}
+
+func fallbackVersion(v string) string {
+	if v == "" {
+		return "?.?.?"
+	}
+	return v
 }
 
 func init() {
 	rootCmd.AddCommand(searchCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// searchCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
